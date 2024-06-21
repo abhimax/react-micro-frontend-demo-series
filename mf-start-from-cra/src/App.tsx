3290176c369c689c553a00623150b19fb90e256c
Abhiman Ranaweera <abhiman.max@gmail.com>
2024-06-21 18:39:08 +0530

    refactor(App): refactor app to typescript react app

--- mf-start-from-cra/src/App.tsx
+++ mf-start-from-cra/src/App.tsx
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Micro Frontend App from CRA</h1>
+      <h1>Micro Frontend React App</h1>
+      <p>Based on CRA</p>
     </div>
   );
 }
