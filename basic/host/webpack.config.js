e19ff2dbc11a999134feda2f0343902aed437b45
Abhiman Ranaweera <abhiman.max@gmail.com>
2024-06-24 05:44:04 +0530

    chore(host mf): setup  host mf

---
+++ basic/host/webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin"); 
+const path = require("path");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./public/index.html",
+  filename: "./index.html"
+});
+module.exports = {
+  mode: 'development',
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    port: 3000,
+    historyApiFallback:{
+      index:'/public/index.html'
+    },
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader"
+      }
+    }
+    ]
+  },
+  plugins: [
+    htmlPlugin 
+  ]
+};
