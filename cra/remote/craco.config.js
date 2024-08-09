dd23faf1f717fd550574495d906137ef7df4fda2
Abhiman Ranaweera <abhiman.max@gmail.com>
2024-08-09 06:28:51 +0530

    feat: add craco cra working project

---
+++ cra/remote/craco.config.js
@@ -0,0 +1,25 @@
+const { ModuleFederationPlugin } = require("webpack").container;
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: 3002,
+  },
+  webpack: {
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "app1",
+        filename: "remoteEntry.js",
+        exposes: {
+          "./App1": "./src/App",
+        },
+        shared: ["react", "react-dom"],
+      }),
+    ],
+    configure: {
+      output: {
+        publicPath: "auto",
+      },
+    },
+  },
+};
